d858c6008e71bf952bb3a982081c640d520a7d9e
Екатерина Зиновьева <katyazinowiewa@gmail.com>
2016-10-12 01:55:55 +0400

    Теперь используется получение дочерних элементов нод через getElementsByTagName(), также теперь меньше вызовов фунции getAttributes у элементов и необходимые аттрибуты получаются через getNamedItem

--- PO43/Zinoveva/wdad/learn/xml/XmlTask.java
+++ PO43/Zinoveva/wdad/learn/xml/XmlTask.java
@@ -34,26 +34,28 @@ public class XmlTask {
     }
 
     private void checkTotalCost() throws IOException {
-        NodeList orderList = doc.getElementsByTagName("order");
+         NodeList orderList = doc.getElementsByTagName("order");
+        NodeList itemsList;
+        NodeList totalCostList;
         int totalCost;
         boolean isTotalCostExist;
         for (int i = 0; i < orderList.getLength(); i++) {
             totalCost = 0;
             isTotalCostExist = false;
-            for (int j = 0; j < orderList.item(i).getChildNodes().getLength(); j++) {
-                if (orderList.item(i).getChildNodes().item(j).getNodeName().equals("item")) {
-                    totalCost += Integer.parseInt(orderList.item(i).getChildNodes().item(j).getAttributes().item(0).getNodeValue());
-                } else if (orderList.item(i).getChildNodes().item(j).getNodeName().equals("totalcost")) {
-                    orderList.item(i).getChildNodes().item(j).setTextContent(String.valueOf(totalCost));
-                    isTotalCostExist = true;
-                    writeDoc();
-                }
+            itemsList = ((Element) orderList.item(i)).getElementsByTagName("item");
+            totalCostList = ((Element) orderList.item(i)).getElementsByTagName("totalcost");
+            for (int j = 0; j < itemsList.getLength(); j++) {
+                totalCost += Integer.parseInt(itemsList.item(j).getAttributes().getNamedItem("cost").getNodeValue());
+            }
+            if (totalCostList.getLength() > 0) {
+                totalCostList.item(0).setTextContent(String.valueOf(totalCost));
+                isTotalCostExist = true;
             }
             if (!isTotalCostExist) {
                 Element totalCostElement = doc.createElement("totalcost");
                 totalCostElement.setTextContent(String.valueOf(totalCost));
                 orderList.item(i).appendChild(totalCostElement);
-                writeDoc();
+                updateDoc();
             }
         }
     }
@@ -76,29 +78,31 @@ public class XmlTask {
         NodeList dateList = doc.getElementsByTagName("date");
         NodeList orderList;
         NodeList orderNodes;
+        NamedNodeMap dateAttributes;
+        int earningsTotal = 0;
         boolean isOfficiantFounded = false;
         for (int i = 0; i < dateList.getLength(); i++) {
-            if ((Integer.valueOf(dateList.item(i).getAttributes().item(0).getNodeValue()) == date.get(Calendar.DAY_OF_MONTH)) &&
-                    (Integer.valueOf(dateList.item(i).getAttributes().item(1).getNodeValue()) == (date.get(Calendar.MONTH))+1) &&
-                    (Integer.valueOf(dateList.item(i).getAttributes().item(2).getNodeValue()) == date.get(Calendar.YEAR))) {
-                orderList = dateList.item(i).getChildNodes();
+            dateAttributes = dateList.item(i).getAttributes();
+            if ((Integer.valueOf(dateAttributes.getNamedItem(DAY_ATTRIBUTE_NAME).getNodeValue()) == date.get(Calendar.DAY_OF_MONTH)) &&
+                    (Integer.valueOf(dateAttributes.getNamedItem(MONTH_ATTRIBUTE_NAME).getNodeValue()) == (date.get(Calendar.MONTH) + 1)) &&
+                    (Integer.valueOf(dateAttributes.getNamedItem(YEAR_ATTRIBUTE_NAME).getNodeValue()) == date.get(Calendar.YEAR))) {
+                orderList = ((Element) dateList.item(i)).getElementsByTagName("order");
                 for (int j = 0; j < orderList.getLength(); j++) {
-                    if (orderList.item(j).getNodeName().equals("order")) {
-                        orderNodes = orderList.item(j).getChildNodes();
-                        for (int k = 0; k < orderNodes.getLength(); k++) {
-                            if ((orderNodes.item(k).getNodeName().equals("officiant")) &&
-                                    orderNodes.item(k).getAttributes().item(1).getNodeValue().equals(officiantSecondName)) {
-                                isOfficiantFounded = true;
-                            }
-                            if ((isOfficiantFounded) && (orderNodes.item(k).getNodeName().equals("totalcost"))) {
-                                return Integer.parseInt(orderNodes.item(k).getTextContent());
-                            }
+                    orderNodes = orderList.item(j).getChildNodes();
+                    for (int k = 0; k < orderNodes.getLength(); k++) {
+                        if (("officiant".equals(orderNodes.item(k).getNodeName())) &&
+                                orderNodes.item(k).getAttributes().getNamedItem(OFFICIANT_SECOND_NAME).getNodeValue().equals(officiantSecondName)) {
+                            isOfficiantFounded = true;
+                        }
+                        if ((isOfficiantFounded) && (orderNodes.item(k).getNodeName().equals("totalcost"))) {
+                            earningsTotal += Integer.parseInt(orderNodes.item(k).getTextContent());
                         }
                     }
+
                 }
             }
         }
-        return 0;
+        return earningsTotal;
     }
 
     /*
@@ -108,16 +112,18 @@ public class XmlTask {
     public void removeDay(Calendar date) throws IOException {
         NodeList dateList = doc.getElementsByTagName("date");
         int datesAmount = dateList.getLength();
+        NamedNodeMap dateAttributes;
         for (int i = 0; i < datesAmount; i++) {
-            if ((Integer.valueOf(dateList.item(i).getAttributes().item(0).getNodeValue()) == date.get(Calendar.DAY_OF_MONTH)) &&
-                    (Integer.valueOf(dateList.item(i).getAttributes().item(1).getNodeValue()) == date.get(Calendar.MONTH)) &&
-                    (Integer.valueOf(dateList.item(i).getAttributes().item(2).getNodeValue()) == date.get(Calendar.YEAR))) {
+            dateAttributes = dateList.item(i).getAttributes();
+            if ((Integer.valueOf(dateAttributes.getNamedItem(DAY_ATTRIBUTE_NAME).getNodeValue()) == date.get(Calendar.DAY_OF_MONTH)) &&
+                    (Integer.valueOf(dateAttributes.getNamedItem(MONTH_ATTRIBUTE_NAME).getNodeValue()) == (date.get(Calendar.MONTH) + 1)) &&
+                    (Integer.valueOf(dateAttributes.getNamedItem(YEAR_ATTRIBUTE_NAME).getNodeValue()) == date.get(Calendar.YEAR))) {
                 dateList.item(i).getParentNode().removeChild(dateList.item(i));
                 i--;
                 datesAmount--;
             }
         }
-        writeDoc();
+        updateDoc();
     }
 
     //Изменяем имя официанта
@@ -125,12 +131,12 @@ public class XmlTask {
         NodeList officiantList = doc.getElementsByTagName("officiant");
         for (int i = 0; i < officiantList.getLength(); i++) {
             NamedNodeMap officiantAttributes = officiantList.item(i).getAttributes();
-            if ((officiantAttributes.item(0).getNodeValue().equals(oldFirstName)) &&
-                    (officiantAttributes.item(1).getNodeValue().equals(oldSecondName))) {
-                officiantAttributes.item(0).setNodeValue(newFirstName);
-                officiantAttributes.item(1).setNodeValue(newSecondName);
+            if ((officiantAttributes.getNamedItem(OFFICIANT_FIRST_NAME).getNodeValue().equals(oldFirstName)) &&
+                    (officiantAttributes.getNamedItem(OFFICIANT_SECOND_NAME).getNodeValue().equals(oldSecondName))) {
+                officiantAttributes.getNamedItem(OFFICIANT_FIRST_NAME).setNodeValue(newFirstName);
+                officiantAttributes.getNamedItem(OFFICIANT_SECOND_NAME).setNodeValue(newSecondName);
             }
         }
-        writeDoc();
+        updateDoc();
     }
 }
