fc06d73bca4de1cb0ae4051bb43bcf75363a67d3
Екатерина Зиновьева <katyazinowiewa@gmail.com>
2016-11-01 23:23:16 +0400

    Выполнено первое и второе задание

--- PO43/Zinoveva/wdad/data/managers/PreferencesManager.java
+++ PO43/Zinoveva/wdad/data/managers/PreferencesManager.java
@@ -1,7 +1,6 @@
 package PO43.Zinoveva.wdad.data.managers;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSOutput;
 import org.w3c.dom.ls.LSSerializer;
@@ -10,10 +9,11 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.util.Properties;
 
 public class PreferencesManager {
     private static PreferencesManager instance;
@@ -31,6 +31,80 @@ public class PreferencesManager {
         return instance;
     }
 
+    private static String getXPath(Node node) {
+        Node parent = node.getParentNode();
+        if (parent == null || parent.getNodeName().equals("#document")) {
+            return node.getNodeName();
+        }
+        return getXPath(parent) + '.' + node.getNodeName();
+    }
+
+    public void setProperty(String key, String value) throws IOException, XPathExpressionException {
+        String xPathKey = "/" + key.replace('.', '/');
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+        XPathExpression xPathExpression = xpath.compile(xPathKey);
+        ((Node) xPathExpression.evaluate(doc, XPathConstants.NODE)).setTextContent(value);
+        writeDoc();
+    }
+
+    public String getProperty(String key) throws XPathExpressionException {
+        String xPathKey = "/" + key.replace('.', '/');
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+        XPathExpression xPathExpression = xpath.compile(xPathKey);
+        return ((Node) xPathExpression.evaluate(doc, XPathConstants.NODE)).getTextContent();
+    }
+
+
+    public Properties getProperties() throws XPathExpressionException {
+        Properties properties = new Properties();
+        String key, value;
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String expression = "//*[not(*)]";
+        NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            key = getXPath(nodeList.item(i));
+            value = getProperty(key);
+            properties.put(key, value);
+        }
+        return properties;
+    }
+
+
+    public void setProperties(Properties prop) throws IOException, XPathExpressionException {
+        for (String key : prop.stringPropertyNames()
+                ) {
+            setProperty(key, prop.getProperty(key));
+        }
+    }
+
+    public void addBindedObject(String name, String className) throws IOException, XPathExpressionException {
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+        String key = "/appconfig/rmi/server/bindedobject[@name='" + name + "']";
+        XPathExpression xPathExpression = xpath.compile(key);
+        Node objectNode = ((Node) xPathExpression.evaluate(doc, XPathConstants.NODE));
+        if (objectNode == null){
+            Element newNode = doc.createElement("bindedobject");
+            newNode.setAttribute("name", name);
+            newNode.setAttribute("class", className);
+            doc.getElementsByTagName("server").item(0).appendChild(newNode);
+            writeDoc();
+        }
+    }
+
+    public void removeBindedObject(String name) throws IOException, XPathExpressionException {
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+        String key = "/appconfig/rmi/server/bindedobject[@name='" + name + "']";
+        XPathExpression xPathExpression = xpath.compile(key);
+        Node objectNode = ((Node) xPathExpression.evaluate(doc, XPathConstants.NODE));
+        objectNode.getParentNode().removeChild(objectNode);
+        writeDoc();
+    }
+
+    @Deprecated
     public boolean isCreateRegistry() {
         NodeList nodeList = doc.getElementsByTagName("createregistry");
         if (nodeList.item(0).getTextContent().equals("yes")) {
@@ -40,6 +114,7 @@ public class PreferencesManager {
         }
     }
 
+    @Deprecated
     public void setCreateRegistry(boolean createRegistry) throws IOException {
         NodeList nodeList = doc.getElementsByTagName("createregistry");
         if (createRegistry) {
@@ -50,39 +125,46 @@ public class PreferencesManager {
         writeDoc();
     }
 
+    @Deprecated
     public String getRegistryAddress() {
         NodeList nodeList = doc.getElementsByTagName("registryaddress");
         return nodeList.item(0).getTextContent();
     }
 
+    @Deprecated
     public void setRegistryAddress(String s) throws IOException {
         NodeList nodeList = doc.getElementsByTagName("registryaddress");
         nodeList.item(0).setTextContent(s);
         writeDoc();
     }
 
+    @Deprecated
     public int getRegistryPort() {
         NodeList nodeList = doc.getElementsByTagName("registryport");
         return Integer.parseInt(nodeList.item(0).getTextContent());
     }
 
+    @Deprecated
     public void setRegistryPort(int registryPort) throws IOException {
         NodeList nodeList = doc.getElementsByTagName("registryport");
         nodeList.item(0).setTextContent(String.valueOf(registryPort));
         writeDoc();
     }
 
+    @Deprecated
     public String getPolicyPath() {
         NodeList nodeList = doc.getElementsByTagName("policypath");
         return nodeList.item(0).getTextContent();
     }
 
+    @Deprecated
     public void setPolicyPath(String s) throws IOException {
         NodeList nodeList = doc.getElementsByTagName("policypath");
         nodeList.item(0).setTextContent(s);
         writeDoc();
     }
 
+    @Deprecated
     public boolean getUseCodeBaseOnly() {
         NodeList nodeList = doc.getElementsByTagName("usecodebaseonly");
         if (nodeList.item(0).getTextContent().equals("yes")) {
@@ -92,6 +174,7 @@ public class PreferencesManager {
         }
     }
 
+    @Deprecated
     public void setUseCodeBaseOnly(boolean useCodeBaseOnly) throws IOException {
         NodeList nodeList = doc.getElementsByTagName("usecodebaseonly");
         if (useCodeBaseOnly) {
@@ -102,11 +185,13 @@ public class PreferencesManager {
         writeDoc();
     }
 
+    @Deprecated
     public String getClassProvider() {
         NodeList nodeList = doc.getElementsByTagName("classprovider");
         return nodeList.item(0).getTextContent();
     }
 
+    @Deprecated
     public void setClassProvider(String classproviderURL) throws IOException {
         NodeList nodeList = doc.getElementsByTagName("classprovider");
         nodeList.item(0).setTextContent(classproviderURL);
--- PO43/Zinoveva/wdad/data/managers/TestPreferencesManager.java
+++ PO43/Zinoveva/wdad/data/managers/TestPreferencesManager.java
@@ -1,16 +1,23 @@
 package PO43.Zinoveva.wdad.data.managers;
 
+import PO43.Zinoveva.wdad.utils.PreferencesConstantManager;
+
 class TestPreferencesManager {
     public static void main(String[] args) {
-        try{
-        PreferencesManager pm = PreferencesManager.getInstance();
-        System.out.println(pm.isCreateRegistry());
-        System.out.println(pm.getRegistryAddress());
-        System.out.println(pm.getRegistryPort());
-        System.out.println(pm.getPolicyPath());
-        System.out.println(pm.getUseCodeBaseOnly());
-        System.out.println(pm.getClassProvider());}
-        catch (Exception e){
+        try {
+            String key = PreferencesConstantManager.CREATE_REGISTRY;
+            PreferencesManager pm = PreferencesManager.getInstance();
+            pm.setProperty(key, "no");
+            System.out.println(pm.getProperty(key));
+            pm.setProperty(key, "yes");
+            pm.getProperties().list(System.out);
+            //pm.getProperties().store(new FileOutputStream("prop.txt"), "something");
+            //Properties test = new Properties();
+            //test.load(new FileInputStream("prop.txt"));
+            //pm.setProperties(test);
+            pm.addBindedObject("test", "testObject");
+            //pm.removeBindedObject("test");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
