ab2b393b32d92ea2e6f6d82b8e8d94d21727ccc5
Екатерина Зиновьева <katyazinowiewa@gmail.com>
2016-11-01 23:11:01 +0400

    Исправлен конфликт

--- PO43/Zinoveva/wdad/Application.java
+++ PO43/Zinoveva/wdad/Application.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PO43.Zinoveva.wdad;
 
-/**
- *
- * @author Админ
- */
 class Application {
     public static void main(String[] args){
         System.out.println("I'm Ekaterina Zinoveva, and I'm not a monkey");
